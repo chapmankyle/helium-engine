af4af05b70cdce5adffd0d3bf55fd4ded324e98d
Kyle Chapman <kyleichapman@gmail.com>
2022-06-20 07:16:33 +0200

    Add LICENSE header to Sandbox

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -1,3 +1,7 @@
+// file      : Sandbox/src/Main.cpp
+// copyright : Copyright (c) 2022-present, Kyle Chapman
+// license   : GPL-3.0; see accompanying LICENSE file
+
 #include <Helium.hpp>
 
 class Sandbox : public Helium::Application {
